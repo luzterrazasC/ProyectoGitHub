338172fa837421874a41696a28d8d6ad573fe3f9
Lucero Terrazas Cendejas <luzterra17@hotmail.com>
2021-07-26 13:26:57 -0500

    Initial Commit

---
+++ ProyectoGitHub/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ProyectoGitHub
+//
+//  Created by Lucero Terrazas Cendejas on 26/07/21.
+//  Copyright © 2021 Erika Lucero Terrazas Cendejas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
